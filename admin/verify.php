a8ec970700aa124be5422e1482eb798cb0620bc8
Geejay Bathan <geraldo.bathanjr@cebupacificair.com>
2023-10-05 09:32:20 +0800

    [Admin] - Verify Account

---
+++ admin/verify.php
@@ -0,0 +1,24 @@
+<?php
+session_start(); 
+include 'db.php';
+
+if($_GET['secret'])
+{
+ 
+  $email = base64_decode($_GET['secret']);
+  $verify = mysqli_query($link,"update admin set status='1' where email='$email'");
+  if($verify > 0)
+  {
+    echo "<script>";
+    echo "self.location='index.php?msg=<font color=green>Your account is activated!</font>';";
+    echo "</script>";
+  }
+  else
+  {
+    echo "<script>";
+    echo "self.location='index.php?msg=<font color=red>Error while activating your account!</font>';";
+    echo "</script>";
+  }
+}
+?>
+
