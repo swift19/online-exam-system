7ca267db15cbb8da2e9097f2db03a4d3ce8a938c
Geejay Bathan <geraldo.bathanjr@cebupacificair.com>
2023-12-09 11:33:24 +0800

    [Admin] registration email already exist

--- admin/registration.php
+++ admin/registration.php
@@ -42,7 +42,7 @@
                             if ($_POST['name'] != "") {                                        
                                 
                                 $email = $_POST['email'];
-                                $query = mysqli_query($link, "select * from student where email = '$email'");
+                                $query = mysqli_query($link, "select * from admin where email = '$email'");
                                 if(filter_var($email, FILTER_VALIDATE_EMAIL  && mysqli_num_rows($query) < 1)){
                                     $name = $_POST['name'];
                                     $username = $_POST['username'];
